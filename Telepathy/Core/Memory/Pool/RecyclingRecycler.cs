dd2395a0d971b15a593defde2d63d86c3ccce932
Toni Miguel <tmiguel@vitalseguro.com>
2017-06-16 14:59:10 +0200

    sdfsdf

--- Telepathy/Core/Memory/Pool/RecyclingRecycler.cs
+++ Telepathy/Core/Memory/Pool/RecyclingRecycler.cs
@@ -1,95 +1,117 @@
 ﻿using System;
 using System.Collections.Generic;
-using Telepathy.Core.Memory.Pool;
 using Telepathy.Core.Util;
 
 // A RecyclingRecycler is an ArraySegmentRecycler which actually pools arrays, in contrast
  // with a WastefulRecycler.  
 
-public class RecyclingRecycler : IArraySegmentRecycler
+namespace Telepathy.Core.Memory.Pool
 {
-
-    private readonly int _log2OfByteSegmentSize;
-    private readonly int _log2OfLongSegmentSize;
-    private readonly Recycler<long[]> _longSegmentRecycler;
-    private readonly Recycler<byte[]> _byteSegmentRecycler;
-
-    public RecyclingRecycler() : this(11, 8) { }
-
-    public RecyclingRecycler(int log2ByteArraySize, int log2LongArraySize)
-    {
-        _log2OfByteSegmentSize = log2ByteArraySize;
-        _log2OfLongSegmentSize = log2LongArraySize;
-
-        _longSegmentRecycler = new Recycler<long[]>(new Creator<long[]>{ Create = () => new long[(1 << log2LongArraySize) + 1]});
-        _byteSegmentRecycler = new Recycler<byte[]>(new Creator<byte[]> { Create = () => new byte[1 << log2ByteArraySize] });
-
-    }
-
-    public int GetLog2OfByteSegmentSize()
-    {
-        return _log2OfByteSegmentSize;
-    }
-
-    public int GetLog2OfLongSegmentSize()
-    {
-        return _log2OfLongSegmentSize;
-    }
-
-    public long[] GetLongArray()
-    {
-        var arr = _longSegmentRecycler.Get();
-        arr.Fill(0);
-        return arr;
-    }
-
-    private class Recycler<T>
+    public class RecyclingRecycler : IArraySegmentRecycler
     {
 
-        private readonly ICreator<T> _creator;
-        private readonly LinkedList<T> _currentSegments;
-        private readonly LinkedList<T> _nextSegments;
+        private readonly int _log2OfByteSegmentSize;
+        private readonly int _log2OfLongSegmentSize;
+        private readonly Recycler<long[]> _longSegmentRecycler;
+        private readonly Recycler<byte[]> _byteSegmentRecycler;
 
-        public Recycler(ICreator<T> creator)
+        public RecyclingRecycler() : this(11, 8) { }
+
+        public RecyclingRecycler(int log2ByteArraySize, int log2LongArraySize)
         {
-            _currentSegments = new LinkedList<T>();
-            _nextSegments = new LinkedList<T>();
-            _creator = creator;
+            _log2OfByteSegmentSize = log2ByteArraySize;
+            _log2OfLongSegmentSize = log2LongArraySize;
+
+            _longSegmentRecycler = new Recycler<long[]>(new Creator<long[]>{ Create = () => new long[(1 << log2LongArraySize) + 1]});
+            _byteSegmentRecycler = new Recycler<byte[]>(new Creator<byte[]> { Create = () => new byte[1 << log2ByteArraySize] });
         }
 
-        public T Get()
+        public int GetLog2OfByteSegmentSize()
         {
-            if (_currentSegments.Count <= 0) return _creator.Create();
-
-            var first = _currentSegments.First.Value;
-            _currentSegments.RemoveFirst();
-
-            return first;
+            return _log2OfByteSegmentSize;
         }
 
-        public void Recycle(T reuse)
+        public int GetLog2OfLongSegmentSize()
         {
-            _nextSegments.AddLast(reuse);
+            return _log2OfLongSegmentSize;
+        }
+
+        public long[] GetLongArray()
+        {
+            var arr = _longSegmentRecycler.Get();
+            arr.Fill(0);
+            return arr;
+        }
+
+        public void RecycleLongArray(long[] arr)
+        {
+            _longSegmentRecycler.Recycle(arr);
+        }
+
+        public byte[] GetByteArray()
+        {
+            return _byteSegmentRecycler.Get();
+        }
+
+        public void RecycleByteArray(byte[] arr)
+        {
+            _byteSegmentRecycler.Recycle(arr);
         }
 
         public void Swap()
         {
-            foreach (var segment in _nextSegments)
-            {
-                _currentSegments.AddLast(segment);
-            }
-            _nextSegments.Clear();
+            _longSegmentRecycler.Swap();
+            _byteSegmentRecycler.Swap();
         }
-    }
 
-    private interface ICreator<T>
-    {
-        Func<T> Create { get; set; }
-    }
+        private class Recycler<T>
+        {
 
-    private class Creator<T> : ICreator<T>
-    {
-        public Func<T> Create { get; set; }
+            private readonly ICreator<T> _creator;
+            private readonly LinkedList<T> _currentSegments;
+            private readonly LinkedList<T> _nextSegments;
+
+            public Recycler(ICreator<T> creator)
+            {
+                _currentSegments = new LinkedList<T>();
+                _nextSegments = new LinkedList<T>();
+                _creator = creator;
+            }
+
+            public T Get()
+            {
+                if (_currentSegments.Count <= 0) return _creator.Create();
+
+                var first = _currentSegments.First.Value;
+                _currentSegments.RemoveFirst();
+
+                return first;
+            }
+
+            public void Recycle(T reuse)
+            {
+                _nextSegments.AddLast(reuse);
+            }
+
+            public void Swap()
+            {
+                foreach (var segment in _nextSegments)
+                {
+                    _currentSegments.AddLast(segment);
+                }
+                _nextSegments.Clear();
+            }
+        }
+
+        private interface ICreator<T>
+        {
+            Func<T> Create { get; set; }
+        }
+
+        private class Creator<T> : ICreator<T>
+        {
+            public Func<T> Create { get; set; }
+        }
     }
 }    
 
